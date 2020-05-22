11c9a229ead167193f1e5216967f6bde9a22fae3
Mikaela Guerrero <mikaelaguerrero2020@gmail.com>
2020-05-22 00:36:25 -0700

    created skeleton code for data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,11 +1,80 @@
 """
-Goal:
+Goals:
+- Load training data (two folders: original text & corresponding annotations)
+- Tokenize on whitespace
+- Convert tokens to IOB sequences
+- Store each token as row in Pandas dataframe with these columns:
+    - token, IOB, doc_ID (filename), sent_ID (sentence number in document), word_ID (word number in document)
+- Insert Pandas dataframe into SQL database? or into pickle?
 """
 
+import pandas as pd
+###import pymysql
+###import argparse???
 
+class DataLoader:
+
+    def __init__(self, txt_folder, concept_folder):
+        """
+        Initialize class variables.
+
+        :param txt_folder: str containing filepath to unstructured txt folder
+        :param concept_folder: str containing filepath to folder
+                                with corresponding concept annotation files
+        """
+        self.txt_folder = txt_folder
+        self.concept_folder = concept_folder
+        
+        self.data = self.process_txt_folder()
+        self.process_concept_folder()
+        
+    def process_txt_folder(self):
+        """
+        Process all files in folder with raw training data.
+        """
+        df = pd.DataFrame(columns=['token', 'IOB',
+                                'doc_ID', 'sent_ID', 'word_ID'])
+        ### iterate through each file in self.txt_folder
+            ### call parse_txt_file() on each file
+            
+        return df
+
+    def parse_txt_file(self, filepath):
+        """
+        Process training data file.
+        Store tokens, doc_ID, sent_ID, and word_ID in dataframe.
+        """
+        doc_ID = filepath
+        ### open file
+            ### iterate through each sentence
+                ### iterate through each word
+                    ### store entry in dataframe
+
+    def process_concept_folder(self):
+        """
+        Process all files in folder with training data annotations.
+        """
+        ### iterate through each file in self.concept_folder
+            ### call parse_concept_file() on each file
+    
+    def parse_concept_file(self, filepath):
+        """
+        Process training data annotation file.
+        Generate IOB tags for concepts and insert in dataframe.
+        """
 
 def main():
-    ### insert code here
+    ### load small example training data (just to test out code on small subset)
+    example = DataLoader(sys.argv[1], sys.argv[2])
+    df = example.data()
+
+    # # load beth training data
+    # beth = DataLoader(sys.argv[3], sys.argv[4])
+    # # load partners training data
+    # partners = DataLoader(sys.argv[5], sys.argv[6])
+    # # merge beth and partners dataframes into one
+
+    ### pickle or SQL???
 
 if __name__ == "__main__":
     main()
---
+++ src/run_data_loader.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+python3 load_data.py \
+    /home2/mguerr20/i2b2_data/small_examples/txt \
+    /home2/mguerr20/i2b2_data/small_examples/concept #\
+    # /home2/mguerr20/i2b2_data/concept_assertion_relation_training_data/beth/txt \
+    # /home2/mguerr20/i2b2_data/concept_assertion_relation_training_data/beth/concept \
+    # /home2/mguerr20/i2b2_data/concept_assertion_relation_training_data/partners/txt \
+    # /home2/mguerr20/i2b2_data/concept_assertion_relation_training_data/partners/concept \
