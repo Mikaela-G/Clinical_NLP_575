88063b710184c2f8fe26131db86ad05cdfdb484b
Mikaela Guerrero <mikaelaguerrero2020@gmail.com>
2020-05-30 19:41:11 -0700

    added pseudocode for morphological features

--- src/FeatureExtractor.py
+++ src/FeatureExtractor.py
@@ -18,18 +18,26 @@ class FeatureExtractor:
         :param data: Pandas dataframe
         """
         self.data = data
+        # convert tokens column to list
+        self.tokens = list(self.data['token'])        
         ### extract features & update df with results of each function
-        
-        ###self.gram_feats()
+        self.morph_feats()
+        ###self.gram_feats() ### temporarily commented out bc slow
+        self.cont_feats()
+        self.metamap_feats()
+        self.sent_feats()
+        self.skipgram_feats()
 
     def morph_feats(self):
+        # get suffixes/prefixes
+        # check if token is capitalized (True/False)
+        # check if special characters in token (True/False)
+        ## WordAsClass??
         pass ###
     
     def gram_feats(self):
-        # convert tokens column to list
-        tokens = list(self.data['token'])
         # tag tokens for POS
-        pos_list = nltk.pos_tag(tokens)
+        pos_list = nltk.pos_tag(self.tokens)
         pos_list = [tup[1] for tup in pos_list]
         # add POS column to dataframe
         self.data['POS'] = pos_list
